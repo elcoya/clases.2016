adee147b0c0ffd07820ba888dcdd0ec5fa1d4d59
Aníbal A. Lovaglio <alelovaglio@gmail.com>
2016-10-20 22:01:52 -0300

    busqueda generica

--- clase.09/checkout.c
+++ clase.09/checkout.c
@@ -8,9 +8,14 @@ int checkout_serve(checkout_t* checkout) {
 	serve_client(client, &ticket);
 	insert_ticket(&(checkout->tickets), &ticket);
 	
+	return RES_OK;
 }
 
 int serve_client(client_t *client, ticket_t *ticket) {}
 
 int insert_ticket(straight_list_t *tickets, ticket_t *ticket) {}
 
+int buscar_en_lista(straight_list_t *list, 
+	void* target, void* current, 
+	int (*fcomp)(const void*, const void*),
+	straight_list_movement_t *movement) {}
